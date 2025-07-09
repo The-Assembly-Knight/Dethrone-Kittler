412c50c1fda531637ebee0ec1532df203f713501
FallacyDev22 <fallacy2208@gmail.com>
2025-07-10 01:48:37 +0200

    Wall Textures!

--- Scenes/Fallacy/main_f.gd
+++ Scenes/Fallacy/main_f.gd
@@ -2,28 +2,52 @@ extends Node2D
 class_name MainF
 
 @onready var player : Player = $player
-var drawingDistance : float = 0
 @onready var screen : Sprite2D = $CanvasLayer/Screen
 
 var rayList : Array[RayCast2D] = []
-var closerList : Array[float] = []
+var distanceList : Array[float] = []
 
+#Se necesita una lista con un espacio Screen.x * 2
+#Los primeros 320 datos son la distancia de la pared al jugador en ese pixel
+#Los segundos 320 datos son la distancia del raycast que colisionó al borde de la pared
+#Esto se usa para dibujar la textura
 func _ready():
-	drawingDistance = pow(player.maxDistance, 2)
-	closerList.resize(player.screenRes.x)
+	distanceList.resize(player.screenRes.x * 2)
+	setMaxDistance()
 
 func _process(delta: float):
 	pass
 
+#Maxima distancia de visión con los raycast
+func setMaxDistance() -> void:
+	var mat : ShaderMaterial = screen.material
+	mat.set_shader_parameter("maxDistance", player.maxDistance)
+
 func giveInfoToShader() -> void:
-	for i in rayList.size():
+	#Iterar sobre toda la lista de raycasts
+	for i in player.screenRes.x:
 		var ray : RayCast2D = rayList[i]
+		#Extraer la rotación del raycast
 		var angleRotation : float = deg_to_rad(remap(i, 0, rayList.size() - 1, player.FOV/-2, player.FOV/2))
 		if ray.is_colliding():
+			#La distancia de la pared al jugador
 			var distanceToPlayer : float = player.global_position.distance_to(ray.get_collision_point())
-			closerList[i] = distanceToPlayer * cos(angleRotation)
+			#Se añade a la lista
+			#La multiplicación por el coseno del angulo es para corregir el efecto de ojo de pez
+			distanceList[i] = distanceToPlayer * cos(angleRotation)
+			#Aquí se settean los datos para la textura, si el raycast colisiona
+			#con una pared en vertical, se usa el punto de colisión en y % 64
+			#con una pared en horizontal, se usa el punto de colisión en x % 64 
+			#el % es una operación llamada modulo (Busquenla si no saben que hace) 
+			if ray.get_collision_normal().y != 0.0:
+				distanceList[i + player.screenRes.x] = fmod(ray.get_collision_point().x, 64.0)
+			else:
+				distanceList[i + player.screenRes.x] = fmod(ray.get_collision_point().y, 64.0)
+		
+		#Si el raycast no colisiona, la distancia hacia esa "pared" es la maxima posible
 		else:
-			closerList[i] = 120.0
+			distanceList[i] = player.maxDistance
 	
+	#Pasar la información al shader
 	var mat : ShaderMaterial = screen.material
-	mat.set_shader_parameter("closerTo", closerList)
+	mat.set_shader_parameter("distanceToW", distanceList)
--- Scenes/Fallacy/player.gd
+++ Scenes/Fallacy/player.gd
@@ -3,11 +3,18 @@ class_name Player
 
 const screenRes : Vector2 = Vector2(320, 200)
 
+##Distancia maxima de vista, cualquier cosa más allá, será dibujada negra
 @export var maxDistance : int = 100
+##Angulo de vista, habitualmente el angulo de vista humano se encuentra entre 45 y 60 grados
 @export var FOV : int = 60
+##Velocidad de movimiento en pixeles por segundo
 @export var movSpeed : float = 60
+##Velocidad de giro en grados por segundo
 @export var rotationSpeed : float = 60
 
+#Bastante explicativa por si sola, genera todos los raycasts
+#Los genera en un rango de -FOV/2 a FOV/2, por ejemplo, si FOV = 60 grados
+#Entonces los raycasts iran de -30 a 30 grados
 func generateRaycasts() -> void:
 	for i in range(screenRes.x):
 		var ray : RayCast2D = RayCast2D.new()
@@ -19,6 +26,7 @@ func generateRaycasts() -> void:
 func _ready():
 	generateRaycasts()
 
+#Control de movimiento y giro
 func _process(delta: float):
 	var dirMov : float = Input.get_axis("fall_s", "fall_w")
 	velocity = (dirMov * movSpeed) * Vector2.from_angle(rotation)
