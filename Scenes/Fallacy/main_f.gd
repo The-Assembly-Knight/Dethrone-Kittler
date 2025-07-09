ab7174a9d97114cadc90c4e24ebe58c3097d5c2c
FallacyDev22 <fallacy2208@gmail.com>
2025-07-10 00:32:03 +0200

    Más bonito

--- Scenes/Fallacy/main_f.gd
+++ Scenes/Fallacy/main_f.gd
@@ -6,7 +6,7 @@ var drawingDistance : float = 0
 @onready var screen : Sprite2D = $CanvasLayer/Screen
 
 var rayList : Array[RayCast2D] = []
-var closerList : Array[Color] = []
+var closerList : Array[float] = []
 
 func _ready():
 	drawingDistance = pow(player.maxDistance, 2)
@@ -18,12 +18,12 @@ func _process(delta: float):
 func giveInfoToShader() -> void:
 	for i in rayList.size():
 		var ray : RayCast2D = rayList[i]
+		var angleRotation : float = deg_to_rad(remap(i, 0, rayList.size() - 1, player.FOV/-2, player.FOV/2))
 		if ray.is_colliding():
-			var distanceToPlayer : float = player.global_position.distance_squared_to(ray.get_collision_point())
-			var remapedValue : int = remap(distanceToPlayer, 0, drawingDistance, 255, 0)
-			closerList[i] = Color8(remapedValue, remapedValue, remapedValue)
+			var distanceToPlayer : float = player.global_position.distance_to(ray.get_collision_point())
+			closerList[i] = distanceToPlayer * cos(angleRotation)
 		else:
-			closerList[i] = Color.BLACK
+			closerList[i] = 120.0
 	
 	var mat : ShaderMaterial = screen.material
-	mat.set_shader_parameter("colors", closerList)
+	mat.set_shader_parameter("closerTo", closerList)
